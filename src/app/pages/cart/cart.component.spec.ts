db434b821cce7e0209463250d349e366da9aaf0a
Diego Pagini <diego.pagini@neoris.com>
2023-06-29 17:32:08 +0200

    tests

--- src/app/pages/cart/cart.component.spec.ts
+++ src/app/pages/cart/cart.component.spec.ts
@@ -1,9 +1,33 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Book } from 'src/app/models/book.model';
 import { BookService } from 'src/app/services/book.service';
 
 import { CartComponent } from './cart.component';
+
+const bookList: Book[] = [
+  {
+    id: 'test',
+    name: 'test',
+    author: 'test',
+    isbn: 'test',
+    description: 'test',
+    photoUrl: 'test',
+    price: 10,
+    amount: 2,
+  },
+  {
+    id: 'test1',
+    name: 'test1',
+    author: 'test1',
+    isbn: 'test1',
+    description: 'test1',
+    photoUrl: 'test1',
+    price: 15,
+    amount: 3,
+  },
+];
 
 describe('CartComponent', () => {
   let component: CartComponent;
@@ -11,8 +35,8 @@ describe('CartComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [HttpClientTestingModule],
       declarations: [CartComponent],
+      imports: [HttpClientTestingModule],
       providers: [BookService],
       schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA],
     }).compileComponents();
@@ -26,5 +50,10 @@ describe('CartComponent', () => {
 
   it('should create the CartComponent', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('getTotalPrice should retruns an amount', () => {
+    const result = component.getTotalPrice(bookList);
+    expect(result).toBeGreaterThan(0);
   });
 });
